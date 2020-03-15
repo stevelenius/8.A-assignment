a86183cc13c39bf94360200b0cfa9a067594100b
stevelenius <44826562+stevelenius@users.noreply.github.com>
2020-03-15 15:15:39 -0500

    interim commit
    1. added images; 2. deleted UID (unique identifier) because not needed. Still working on verifying song field is not empty.

--- 8.A-assignment.js
+++ 8.A-assignment.js
@@ -8,46 +8,46 @@ let recordsArray = [
   {
   song: "Rhapsody in Blue",
   artist: "George Gershwin with Paul Whiteman and His Orchestra",
+  image: 'https://slenius.studio.mcad.edu/webprog/8.A-assignment/images/RhapsodyGershwin-200x200.png',
   label: "Victor",
   recordNo: "55225",
-  year: "1924",
-  uid: "001"
+  year: "1924"
   },
   
   {
   song: "Jazz Me Blues",
   artist: "Bix Beiderbecke and his Gang",
+  image: 'https://slenius.studio.mcad.edu/webprog/8.A-assignment/images/JazzMeBeiderbecke-200x200.png',
   label: "Vocalion",
   recordNo: "3042",
-  year: "1927",
-  uid: "002"
+  year: "1927"
 },
     
   {
   song: "Mood Indigo",
   artist: "Duke Ellington & His Famous Orchestra (vocal by Ivie Anderson)",
+  image: 'https://slenius.studio.mcad.edu/webprog/8.A-assignment/images/MoodIndigoEllington-200x200.png',
   label: "Columbia",
   recordNo: "35427",
-  year: "1940",
-  uid: "003"
+  year: "1940"
   },
   
   {
   song: "More Than You Know",
   artist: "Billie Holiday",
+  image: 'https://slenius.studio.mcad.edu/webprog/8.A-assignment/images/MoreThanYouKnowHoliday-200x200.png',
   label: "Columbia",
   recordNo: "36117",
-  year: "1941",
-  uid: "004"
+  year: "1941"
   },
   
   {
   song: "Down For Double",
   artist: "Count Basie and His Orchestra",
+  image: 'https://slenius.studio.mcad.edu/webprog/8.A-assignment/images/DownForDoubleBasie-200x200.png',
   label: "Okeh",
   recordNo: "6584",
-  year: "1942",
-  uid: "005"
+  year: "1942"
   }  
 
 ];
@@ -59,15 +59,22 @@ const vm = new Vue({
     newRecordsObj: {
       song: '',
       artist: '',
+      image: '',
       label: '',
       recordNo: '',
       year: '',
-      uid: null
     },
     records: recordsArray
   },
   methods: {
+    checkSong: function () {
+      console.log(vm.newRecordsObj.song);
+      if(vm.newRecordsObj.song === "") {
+        alert("No song name given");
+      }
+    },
     submitHandler: () => {
+      vm.checkSong();
       console.log('submitted');
       vm.records = vm.records.concat(vm.newRecordsObj);
       vm.resetForm();
@@ -76,10 +83,10 @@ const vm = new Vue({
       vm.newRecordsObj = {
       song: '',
       artist: '',
+      image: '',
       label: '',
       recordNo: '',
       year: '',
-      uid: null
       };
     },
     deleteItem: item => {
